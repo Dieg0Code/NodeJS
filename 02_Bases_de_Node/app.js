19c28a3c3f3e138aaeae3074f9836e1bdbaa2e7b
Dieg0Code <diegoobando20@gmail.com>
2022-01-02 18:16:02 -0300

    importar archivos de nuestro proyecto

--- 02_Bases_de_Node/app.js
+++ 02_Bases_de_Node/app.js
@@ -1,24 +1,12 @@
-const fs = require('fs');
+const { crearArchivo } = require('./helpers/multiplicar');
 
 console.clear();
-console.log('=========================');
-console.log('      Tabla del 5');
-console.log('=========================');
 
 
-const base = 3;
-let salida = '';
 
-for (let i = 1; i <= 10; i++) {
-    
-    salida += `${base} x ${i} = ${base * i} \n`;
-    
-}
+const base = 9;
 
-console.log(salida);
 
-fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
-    if (err) throw err;
-
-    console.log(`Tabla del ${base} guardada`);
-})
+crearArchivo(base)
+    .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
+    .catch(err => console.log(err));
